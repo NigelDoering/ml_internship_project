c93849e4deda7fa1ec8f2f7665fad735e335d3aa
CrisHdez8 <cristian.hernandez8@outlook.com>
2022-06-08 13:54:02 -0700

    NewNotebook

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,25 @@
+print("Hello World")
+print("Hello Nigel")
+print(10 + 5)
+print(10 * 3)
+name = input("What is your name? ")
+time = int(input("What time is it? "))
+if time > 6:
+    print("Its getting late.")
+    print("We should start heading home.")
+elif time < 6:
+    print("We can stay a little longer.")
+temperature = input("Do you know todays temperature? ")
+    if temperature > 30:
+        print("Its a hot day")
+        print("Drink plenty of water")
+    elif temperature > 20:
+        print("Its a nice day")
+    elif temperature > 10:
+        print("Its a bit cold")
+    else:
+        print("Its cold")
+x = 3 > 2 
+print(x)
+x = (10 + 3) * 2
+print(x)
