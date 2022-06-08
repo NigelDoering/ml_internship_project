d160b2b72a60e296691292c5b1690c7902da5b9a
CrisHdez8 <cristian.hernandez8@outlook.com>
2022-06-08 12:22:28 -0700

    Time

--- main.py
+++ main.py
@@ -3,4 +3,9 @@ print("Hello Nigel")
 print(10 + 5)
 print(10 * 3)
 name = input("What is your name? ")
-input("What time is it? ")
+time = int(input("What time is it? "))
+if time > 6:
+    print("Its getting late.")
+    print("We should start heading home.")
+elif time < 6:
+    print("We can stay a little longer.")
